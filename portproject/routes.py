eaf8bb4f308a33e523ea708a5706930d021d5cb2
Lucas Comaru <lucascomaru20@gmail.com>
2022-11-30 21:00:07 -0300

    Formulando a base do site

--- portproject/routes.py
+++ portproject/routes.py
@@ -4,4 +4,10 @@ from portproject import app
 
 @app.route('/')
 def home ():
-    return render_template('homepage.html')
+    foto_perfil = url_for('static', filename='images/fotolucas.jpg')
+    github = url_for('static', filename='images/github2.png')
+    contato = url_for('static', filename='images/contato.png')
+    cursos = url_for('static', filename='images/cursos.png')
+    projetos = url_for('static', filename='images/projetos.png')
+    return render_template('homepage.html', foto_perfil=foto_perfil, github=github, contato=contato
+                           ,cursos=cursos, projetos=projetos)
